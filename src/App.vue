bcc1490a182f20fbcc8c4787bd419b193e4b907e
ZaferGok <41114328+zzafergok@users.noreply.github.com>
2022-06-24 20:59:44 +0300

    fixed css bug and removed unncessary lines

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
-    <router-link to="/categories">Categories</router-link>
+    <!-- <router-link to="/">Home</router-link>
+    <router-link to="/categories">Categories</router-link> -->
     <router-view></router-view>
   </div>
 </template>
@@ -12,7 +12,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scope>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,5 +20,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
 }
 </style>
