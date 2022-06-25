5b3a8532dd71595219e99d676f1e6a7a45b562aa
Zafer Gok <zfrgk96@gmail.com>
2022-06-26 00:00:53 +0300

    added categories route

--- src/router/index.js
+++ src/router/index.js
@@ -4,25 +4,82 @@ import Home from "../pages/Home.vue";
 
 Vue.use(VueRouter);
 
+const category = [
+  "business",
+  "entertainment",
+  "general",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
+
 const routes = [
   {
     path: "/",
     name: "home-page",
     component: Home,
   },
+  // {
+  //   path: "/business",
+  //   name: "categories-page",
+  //   component: () =>
+  //     import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
+  // },
+  // {
+  //   path: "/entertainment",
+  //   name: "categories-page",
+  //   component: () =>
+  //     import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
+  // },
+  // {
+  //   path: "/general",
+  //   name: "categories-page",
+  //   component: () =>
+  //     import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
+  // },
+  // {
+  //   path: "/health",
+  //   name: "categories-page",
+  //   component: () =>
+  //     import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
+  // },
+  // {
+  //   path: "/science",
+  //   name: "categories-page",
+  //   component: () =>
+  //     import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
+  // },
+  // {
+  //   path: "/sports",
+  //   name: "categories-page",
+  //   component: () =>
+  //     import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
+  // },
+  // {
+  //   path: "/technology",
+  //   name: "categories-page",
+  //   component: () =>
+  //     import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
+  // },
   {
     path: "/:id",
     name: "details-page",
     component: () =>
       import(/* webpackChunkName: "details" */ "../pages/Details.vue"),
   },
-  {
-    path: "/categories",
+];
+
+category.map((item) => {
+  const newCategories = {
+    path: `/${item}`,
     name: "categories-page",
     component: () =>
       import(/* webpackChunkName: "categories" */ "../pages/Categories.vue"),
-  },
-];
+  };
+
+  routes.unshift(newCategories);
+});
 
 const router = new VueRouter({
   mode: "history",
